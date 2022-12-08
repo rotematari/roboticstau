82d237262ad6f1dc83fc2e9ffe1d0bd6cc842bd7
rotematari <rotematari@mail.tau.ac.il>
2022-12-08 12:40:18 +0200

    NN_softmax + data loader

--- clasifiers/NN_softmax.py
+++ clasifiers/NN_softmax.py
@@ -16,34 +16,20 @@ hidden_size = 40
 num_classes = 4
 num_epochs = 15
 batch_size = 30
-learning_rate = 0.0001
+learning_rate = 0.01
 
-# MNIST dataset
-# train_dataset = torchvision.datasets.MNIST(root='./data',
-#                                            train=True,
-#                                            transform=transforms.ToTensor(),
-#                                            download=True)
-#
-# test_dataset = torchvision.datasets.MNIST(root='./data',
-#                                           train=False,
-#                                           transform=transforms.ToTensor())
+
 # pointing dataset
 point_data_train = data_loader.Data(train=True, dirpath=dirpath)
 point_data_test = data_loader.Data(train=False, dirpath=dirpath)
 
-# point_data.X_train = torch.from_numpy(point_data.X_train)
-# point_data.X_test = torch.from_numpy(point_data.X_test)
-# point_data.Y_train = torch.from_numpy(point_data.Y_train)
-# point_data.Y_test = torch.from_numpy(point_data.Y_test)
-# print(point_data.X_train.shape)
+
 
 # Data loader
 train_loader = torch.utils.data.DataLoader(dataset=point_data_train,
                                            batch_size=batch_size,
                                            shuffle=True)
-# train_loader_y = torch.utils.data.DataLoader(dataset=point_data.Y_train,
-#                                            batch_size=batch_size,
-#                                            shuffle=False)
+
 test_loader = torch.utils.data.DataLoader(dataset=point_data_test,
                                           batch_size=batch_size,
                                           shuffle=True)
@@ -68,11 +54,14 @@ class NeuralNet(nn.Module):
         super(NeuralNet, self).__init__()
         self.input_size = input_size
         self.l1 = nn.Linear(input_size, hidden_size)
+        # self.l1 = nn.ReLU()
         self.relu = nn.ReLU()
         self.l2 = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
         out = self.l1(x)
+        out = self.relu(out)
+        out = self.relu(out)
         out = self.relu(out)
         out = self.l2(out)
         # no activation and no softmax at the end
@@ -110,11 +99,15 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
 
-        if (i + 1) % 100 == 0:
+        if (i + 1) % 40 == 0:
             print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{n_total_steps}], Loss: {loss.item():.4f}')
+        if loss <= 0.05:
+            # print("loss small ")
+            break
 
 # Test the model
 # In test phase, we don't need to compute gradients (for memory efficiency)
+print(test_loader)
 with torch.no_grad():
     n_correct = 0
     n_samples = 0
@@ -123,10 +116,12 @@ with torch.no_grad():
         labels = labels.to(device)
         X = X.to(device)
         outputs = model(X)
+
         # max returns (value ,index)
         _, predicted = torch.max(outputs.data, 1)
         n_samples += labels.size(0)
         n_correct += (predicted == labels).sum().item()
-
+    print(outputs.data)
+    print(torch.max(outputs.data))
     acc = 100.0 * n_correct / n_samples
     print(f'Accuracy of the network on the 10000 test images: {acc} %')
---
+++ learn/test.py
@@ -0,0 +1,12 @@
+x = 1
+y = 2
+
+
+def add(x, y):
+    return x + y
+
+
+z = (x == y)
+
+print(z)
+
