e568ce24eb17b98063097bc57de53cd5bb6061ef
rotematari <rotem.atri@gmail.com>
2023-01-03 17:45:30 +0200

    3/1 new change

--- paramaters.py
+++ paramaters.py
@@ -1,5 +1,5 @@
 class parameters():
 
-    dirpath = r'/home/roblab15/Documents/FMG_project/data'
+    dirpath = r'/home/roblab20/Documents/rotem/data'
     items = ['S2', 'S3', 'S4', 'S5', 'S6']
     model_path = r'/home/roblab15/Documents/FMG_project/models'
